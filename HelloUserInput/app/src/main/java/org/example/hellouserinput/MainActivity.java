3df5445e95b56802cd5216d7bc47c49b1b94caf8
JJediDR <jeanluc.delonyrobert@gmail.com>
2016-06-09 12:19:26 -0500

    add existing file

---
+++ HelloUserInput/app/src/main/java/org/example/hellouserinput/MainActivity.java
@@ -0,0 +1,30 @@
+package org.example.hellouserinput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private TextView Salut;
+    private EditText Name;
+
+    public void Button(View v) {
+        Button button = (Button) v;
+
+        Salut = (TextView)findViewById(R.id.Hello);
+        Name = (EditText)findViewById(R.id.Name);
+        Salut.setText("Hello " + Name.getText() + "!");
+    }
+
+
+
+}
