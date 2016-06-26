fde631ce0a19e76306ba4523bf585d453e6c4864
JJediDR <jeanluc.delonyrobert@gmail.com>
2016-06-26 12:26:45 -0500

    Update to existing File

---
+++ HelloUserInput/app/src/main/java/org/example/hellouserinput/Adapter/NamesAdapter.java
@@ -0,0 +1,10 @@
+package org.example.hellouserinput.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+import org.example.hellouserinput.R;
+
+import java.util.List;
+import java.util.jar.Attributes;
+
--- HelloUserInput/app/src/main/java/org/example/hellouserinput/MainActivity.java
+++ HelloUserInput/app/src/main/java/org/example/hellouserinput/MainActivity.java
@@ -1,11 +1,22 @@
 package org.example.hellouserinput;
 
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,16 +24,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView Names = (RecyclerView)findViewById(R.id.NameList);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        llm.canScrollVertically();
+
+        Names.setLayoutManager(llm);
     }
-    private TextView Salut;
+
+    public abstract static class NamesAdapter extends
+            RecyclerView.Adapter<NamesAdapter.ViewHolder> {
+
+        // Provide a direct reference to each of the views within a data item
+        // Used to cache the views within the item layout for fast access
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            // Your holder should contain a member variable
+            // for any view that will be set as you render a row
+            public TextView NameTextView;
+
+
+            // We also create a constructor that accepts the entire item row
+            // and does the view lookups to find each subview
+            public ViewHolder(View itemView) {
+                // Stores the itemView in a public final member variable that can be used
+                // to access the context from any ViewHolder instance.
+                super(itemView);
+
+                NameTextView = (TextView)itemView.findViewById(R.id.NameView);
+            }
+        }
+    }
+
+    public abstract class NameAdapter extends RecyclerView.Adapter<NamesAdapter.ViewHolder> {
+
+        public NameAdapter(List<Attributes.Name> Names) {
+            Names = NameArray;
+
+
+        }
+
+
+    }
+
+
+
+
+        private TextView Salut;
     private EditText Name;
+    public ArrayList NameArray = new ArrayList(1);
 
-    public void Button(View v) {
-        Button button = (Button) v;
 
+
+
+
+
+    public ArrayList Button(View v) {
+        Button EnterButton = (Button) v;
+
+
+        NameArray.add(Name);
         Salut = (TextView)findViewById(R.id.Hello);
         Name = (EditText)findViewById(R.id.Name);
         Salut.setText(getString(R.string.Hello) + " " + Name.getText());
+
+        return NameArray;
     }
 
 
